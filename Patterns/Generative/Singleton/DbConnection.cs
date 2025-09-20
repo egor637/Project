3e795276c2a878c520190d38a52ec1320eb3d71c
egor637 <e.baskakov.2003@gmail.com>
2025-09-20 11:53:39 +0300

    Add files via upload

---
+++ Patterns/Generative/Singleton/DbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Generative.Singleton
+{
+    internal class DbConnection
+    {
+        static Connection Connection;
+        public static Connection GetConnection()
+        {
+            if (Connection == null)
+            {
+                Connection = new Connection();
+            }
+            return Connection;
+        }
+
+    }
+}
